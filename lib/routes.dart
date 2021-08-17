ad3434a12ef016dead1db538ece45523dc05fb13
Kochetov.Pavel <kochetov.pavel@wildberries.ru>
2021-08-17 16:35:07 +0300

    init

---
+++ lib/routes.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+import 'package:odo24_mobile/screens/splash/splash.screen.dart';
+
+final Map<String, WidgetBuilder> routes = {
+  '/': (context) => SplashScreen(),
+};
